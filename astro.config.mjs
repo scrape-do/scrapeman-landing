6e13495ad2267ae8cdc982b18386da9f66dcb64e
Fatih Kaya <1994274@gmail.com>
2026-06-23 17:53:04 +0300

    use wrangler for production deployments

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,11 @@
-import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
-import react from '@astrojs/react';
-import sitemap from '@astrojs/sitemap';
+import react from "@astrojs/react";
+import sitemap from "@astrojs/sitemap";
+import tailwind from "@astrojs/tailwind";
+import { defineConfig } from "astro/config";
 
 export default defineConfig({
-  site: 'https://scrapeman.app',
-  integrations: [tailwind(), react(), sitemap()],
-  output: 'static',
-  trailingSlash: 'never',
+	site: "https://scrapeman.app",
+	integrations: [tailwind(), react(), sitemap()],
+	output: "static",
+	trailingSlash: "never",
 });
